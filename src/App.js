fa491afe614a70af2c9947d955c8dfb8686ac576
ProLyfe <rayane.prolyfe@gmail.com>
2021-10-01 01:12:36 +0200

    panel

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [mail, setMail] = useState([
+    'test@test.com',
+    'Rayane.prolyfe@gmail.com',
+    'Marie.andon@gmail.com',
+  ]);
+
+  useEffect(() => {
+    fetch('/mails')
+      .then(res => res.json())
+      .then(res => console.log(res))
+  }, [])
+
   return (
     <div className="App">
+      <h1>Pannel Admin | BootCode</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       {mail.map(email => <p>{email}</p>)}
       </header>
     </div>
   );
